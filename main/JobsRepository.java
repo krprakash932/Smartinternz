5a692f1576b66f69739735392d2004ab5dbe760a
krprakash932 <52601039+krprakash932@users.noreply.github.com>
2021-04-16 14:31:30 +0530

    Files attached

---
+++ main/JobsRepository.java
@@ -0,0 +1,7 @@
+package com.prakashcodes.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobsRepository extends JpaRepository{
+
+}
---
+++ main/PrakashJobportalApplication.java
@@ -0,0 +1,13 @@
+package com.prakashcodes.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrakashJobportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrakashJobportalApplication.class, args);
+	}
+
+}
